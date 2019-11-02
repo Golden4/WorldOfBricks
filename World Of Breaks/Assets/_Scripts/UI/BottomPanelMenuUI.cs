775ac8346b0a17f9000d3471bfb79b1d033c6cb2
GoldenA <g.axe@list.ru>
2019-11-03 02:54:21 +0500

    NewDesign

---
+++ World Of Breaks/Assets/_Scripts/UI/BottomPanelMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomPanelMenuUI : MonoBehaviour
+{
+    public BottomPanelBtnInfo[] btnInfos;
+    public Image selectedBtn;
+    Text selectedBtnText;
+    Image selectedBtnSprite;
+
+    void Start()
+    {
+        selectedBtnText = selectedBtn.transform.Find("Text").GetComponent<Text>();
+        selectedBtnSprite = selectedBtn.transform.Find("Image").GetComponent<Image>();
+    }
+
+    [System.Serializable]
+    public class BottomPanelBtnInfo
+    {
+        public Sprite sprite;
+        public string name;
+        public ScreenController.GameScreen screen;
+    }
+
+    public void ChangeBtn(int index)
+    {
+
+    }
+
+    public void ChangeBtn(BottomPanelBtnInfo info)
+    {
+
+    }
+}
